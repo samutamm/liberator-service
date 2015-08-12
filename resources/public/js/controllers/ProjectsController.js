08b6f828a849300a920e88d92bae0c1048fc40c7
samutamm <samutamm@gmail.com>
2015-08-12 12:33:50 +0300

    Search by one tag works.

--- resources/public/js/controllers/ProjectsController.js
+++ resources/public/js/controllers/ProjectsController.js
@@ -1,7 +1,11 @@
 angular.module('samutammApp').controller('ProjectsController', function (Tag, Project, $scope) {
+    $scope.search = {};
     $scope.projects = Project.query();
     $scope.imageUrl = function(project) {
         return "https://s3.eu-central-1.amazonaws.com/samutamm-images/" + project.image;
     };
     $scope.allTags = Tag.query();
+    $scope.tagAdded = function(newTag) {
+      $scope.search.tags = newTag.text;
+    }
 });
