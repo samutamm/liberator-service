59529db365cbd71ad1492c3cb3ebe7176b50c34e
samutamm <samutamm@gmail.com>
2015-09-06 16:56:48 +0200

    Delete button.

--- resources/public/js/controllers/ProjectShowController.js
+++ resources/public/js/controllers/ProjectShowController.js
@@ -1,8 +1,15 @@
-angular.module('samutammApp').controller('ProjectShowController', function (Project, $scope, $routeParams) {
+angular.module('samutammApp').controller('ProjectShowController', function ($location, Auth, Project, $scope, $routeParams) {
     Project.get({id: $routeParams.id}).$promise.then(function(response) {
       $scope.imageUrl = "https://s3.eu-central-1.amazonaws.com/samutamm-images/" + response.image;
       $scope.project = response;
       $scope.github = JSON.parse(response.links).github;
       $scope.tags = response.tags.split(";");
     });
+
+    $scope.deleteProject = function() {
+      new Auth();
+      Project.delete({id: $routeParams.id}).$promise.then(function(response) {
+        $location.path("/projects");
+      });
+    }
 });
